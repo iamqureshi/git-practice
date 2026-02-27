969acb5fb090e1637cd800f7d8417692c3341237
Samir Shaikh <samirshaikh.co.in@gmail.com>
2026-02-27 23:12:30 +0530

    feat(login-ui): basic login form

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <form>
+        <input type="email" />
+        <input type="password" />
+        <button>Login</button>
+      </form>
+    </>
+  );
+};
+
+export default Login;
