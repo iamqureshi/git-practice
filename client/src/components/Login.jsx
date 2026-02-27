0065cf0800626d2c562882716cfa6ec2d89fab56
Samir Shaikh <samirshaikh.co.in@gmail.com>
2026-02-27 23:39:58 +0530

    latest one from me

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,8 +1,7 @@
 export default function Login() {
   return (
     <form>
-      <input type="password" />
-      <input type="password" />
+      <input type="sameer"/>
       <input type="password" />
       <input type="password" />
     </form>
